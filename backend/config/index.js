8e0fa5b91e3a53bcbbba29b7087fb8a289357bdd
Dor Vaknin <dor9879875@gmail.com>
2019-07-17 21:42:20 +0300

    Created a basic backend folder, structure of routing, structure of Schemas, connection to mongoDB and so.

---
+++ backend/config/index.js
@@ -0,0 +1,8 @@
+var configValues = require('./config');
+
+module.exports = {
+    getDbConnectionString: function () {
+        return 'mongodb + srv://DorVaknin:Vaknin123@@bisbuit-w7m9v.mongodb.net/test?retryWrites=true&w=majority';
+    }
+}
+
